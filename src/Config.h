8f6f2d944e6f011192f25c997a05b76f7cfec9ee
Zachary Roberts <zroberts1@ycp.edu>
2023-02-18 02:58:33 -0500

    Initial Support for ESP32-S3-DevKitM-1
    Ported Code over from ESP32-S2 to ESP32-S3
    - Updated platformio.ini 
    - Created Buffer Struct
    - EMG signal data is now stored in a single deque buffer
    - Increased Ram of each task to 2048 Kb
    - Added ESP32S3 support to ESP32AnalogRead library
    - test directory was added. This is required to use platformio serial monitor.

--- src/Config.h
+++ src/Config.h
@@ -3,13 +3,13 @@
  *
  *  Created on: 06 Dec, 2022
  *      Author: Zachary Roberts (zroberts1@ycp.edu)
- * 
+ *
  *      This file is used to store defines and set configurations for the main
- *      proccess of the application. 
+ *      proccess of the application.
  */
 
 // Sets max buffer size to 5 seconds, 1 sec = 100 samples
-#define MAX_DUFFER_SIZE 499 
+#define MAX_BUFFER_SIZE 1000
 
 // Defines Constant for PWM Channels
 // Used to identify which finger an object is to operate on
@@ -19,8 +19,17 @@
 #define FINGER_RING_CHANNEL 3
 #define FINGER_PINKY_CHANNEL 4
 
-// Saftey Threshold for Servo Current Consumption 
-#define MAX_SERVO_CURRENT 1499
+// Saftey Threshold for Servo Current Consumption
+#define MAX_SERVO_CURRENT 500
 
-// Max value for feedback collision is 20000 millivolts
-#define FEEDBACK_THRESHOLD 2000 
+// Buffer structure for storing EMG data
+struct buffer {
+  uint16_t myo1;
+  uint16_t myo2;
+  uint16_t myo3;
+  uint16_t myo4;
+  uint16_t myo5;
+  uint16_t myo6;
+  uint16_t myo7;
+  uint16_t myo8;
+};
