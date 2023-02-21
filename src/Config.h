ccf5a7e90b0171c35e0b91e34a20f56cf9bc941f
Zachary Roberts <zroberts1@ycp.edu>
2023-02-20 21:07:08 -0500

    Updated Config file

--- src/Config.h
+++ src/Config.h
@@ -9,7 +9,7 @@
  */
 
 // Sets max buffer size to 5 seconds, 1 sec = 100 samples
-#define MAX_BUFFER_SIZE 1000
+#define MAX_BUFFER_SIZE 499
 
 // Defines Constant for PWM Channels
 // Used to identify which finger an object is to operate on
@@ -20,16 +20,16 @@
 #define FINGER_PINKY_CHANNEL 4
 
 // Saftey Threshold for Servo Current Consumption
-#define MAX_SERVO_CURRENT 500
+#define MAX_SERVO_CURRENT 930  // 300 mA maximum current draw per servo motor
 
 // Buffer structure for storing EMG data
 struct buffer {
   uint16_t myo1;
   uint16_t myo2;
-  uint16_t myo3;
-  uint16_t myo4;
-  uint16_t myo5;
-  uint16_t myo6;
-  uint16_t myo7;
-  uint16_t myo8;
+  //   uint16_t myo3;
+  //   uint16_t myo4;
+  //   uint16_t myo5;
+  //   uint16_t myo6;
+  //   uint16_t myo7;
+  //   uint16_t myo8;
 };
